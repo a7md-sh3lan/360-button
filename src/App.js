b55bf6bcdcaf61188ba432b66d4b158089abb682
Ahmad Sha'lan <sh3lan@192.168.1.12>
2023-05-12 05:10:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { ConnectButton } from "360dialog-connect-button";
+import styled, { css } from 'styled-components';
+
+
+function App() {
+  const handleCallback = (callbackObject) => {
+    /* The callback function returns the client ID as well as all channel IDs, for which you're enabled to fetch the API key via the Partner API */
+
+    console.log("client ID: "+callbackObject.client)
+    console.log("channel IDs: " + callbackObject.channels);
+  }
+
+  const StyledConnectButton = styled(ConnectButton)`
+    outline: none;
+    background: #ff4369;
+    color: white;
+    padding: 8px 16px;
+    border-radius: 3px;
+    margin-top: 32px;
+    border: none;
+  `;
+
+  return (
+    <div className="App">
+      
+      <div>
+        <StyledConnectButton 
+          partnerId={'f167CmPA'}
+          callback={handleCallback}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
